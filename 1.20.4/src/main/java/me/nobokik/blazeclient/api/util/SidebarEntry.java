36fa25fe714484ade1f51dc798c0828083583769
Tyler <184896961+reallytyler@users.noreply.github.com>
2026-04-14 13:54:35 +0600

    Delete 1.20.4 directory

--- 1.20.4/src/main/java/me/nobokik/blazeclient/api/util/SidebarEntry.java
+++
@@ -1,9 +0,0 @@
-package me.nobokik.blazeclient.api.util;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.text.Text;
-
-@Environment(value=EnvType.CLIENT)
-public record SidebarEntry(Text name, Text score, int scoreWidth) {
-}
